d14f0ac4205c010bc1bcb0ed32ffed0e6916f77e
Domy <wkaifang@gmail.com>
2016-05-16 11:14:41 +0800

    update

--- Front-end/app/index.js
+++ Front-end/app/index.js
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Router, Route, Link } from 'react-router'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { Router, Route, Link } from 'react-router'
+import { createStore } from 'redux'
+// import { Provider } from 'react-redux'
+//
+// import reducers from './reducers'
+// let store = createStore(reducers)
+//
+// import history from './history'
+// import routes from './routes'
+//
+// ReactDOM.render((
+//   <Router history={history} routes={routes} />
+// ), document.body)
 
-import history from './history'
-import routes from './routes'
+function todos(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return state.concat([ action.text ])
+    default:
+      return state
+  }
+}
 
-ReactDOM.render((
-  <Router history={history} routes={routes} />
-), document.body)
+let store = createStore(todos, [ 'Use Redux' ])
+
+store.dispatch({
+  type: 'ADD_TODO',
+  text: 'Read the docs'
+})
+
+console.log(store.getState())
